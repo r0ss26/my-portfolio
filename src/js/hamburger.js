d94a716150d4971241307af83c64e20dafcb7b0f
Ross Baker <rossbaker32@gmail.com>
2020-03-31 11:44:13 +1100

    styling styled contact form initial draft of works page

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,8 +1,4 @@
 document.getElementById("hamburger-menu").addEventListener("click", () => {
   menu = document.getElementById("hamburger-links")
-  if (menu.style.display == "none") {
-    menu.style.display = "block"
-  } else {
-    menu.style.display = "none"
-  }
+  menu.classList.toggle("hidden")
 })

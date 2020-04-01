b9bc9fcb0ab9b299093eb7a06f09ea2f553d252a
Ross Baker <rossbaker32@gmail.com>
2020-04-01 14:29:40 +1100

    blog posts added blog posts

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -2,3 +2,13 @@ document.getElementById("hamburger-menu").addEventListener("click", () => {
   menu = document.getElementById("hamburger-links")
   menu.classList.toggle("hidden")
 })
+
+Array.from(document.getElementsByClassName("see-more")).forEach((el, i, arr) => {
+  el.addEventListener("click", () => {
+    more = el.nextSibling
+    more.classList.toggle("hidden")
+  })
+}
+)
+
+
